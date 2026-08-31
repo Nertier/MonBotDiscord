b62293a11510867ec596bb5466913a42e3254b0c
Nertier <liamlaurent0000@gmail.com>
2026-08-31 10:06:12 +0200

    Add new slash commands for moderation and polls
    Added new commands for clearing messages, warning members, unbanning users, creating polls, and displaying member avatars.

--- register.js
+++ register.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+const { REST, Routes, SlashCommandBuilder, ChannelType } = require('discord.js');
 
 const commands = [
     new SlashCommandBuilder()
@@ -84,11 +84,77 @@ const commands = [
 
     new SlashCommandBuilder()
         .setName('modif')
-        .setDescription('Panneau de configuration du bot (Mods uniquement)')
+        .setDescription('Panneau de configuration du bot (Mods uniquement)'),
+
+    // --- NOUVELLES COMMANDES ---
+    new SlashCommandBuilder()
+        .setName('clear')
+        .setDescription('Supprimer un nombre de messages dans le salon (Mods uniquement)')
+        .addIntegerOption(option =>
+            option.setName('nombre')
+                .setDescription('Nombre de messages à supprimer (1 à 100)')
+                .setRequired(true)
+                .setMinValue(1)
+                .setMaxValue(100)),
+
+    new SlashCommandBuilder()
+        .setName('warn')
+        .setDescription('Avertir un membre et envoyer un rapport (Mods uniquement)')
+        .addUserOption(option =>
+            option.setName('membre')
+                .setDescription('Le membre à avertir')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('raison')
+                .setDescription('Raison du warn')
+                .setRequired(true))
+        .addChannelOption(option =>
+            option.setName('salon')
+                .setDescription('Salon où envoyer le rapport')
+                .addChannelTypes(ChannelType.GuildText)
+                .setRequired(true)),
+
+    new SlashCommandBuilder()
+        .setName('unban')
+        .setDescription('Débannir un utilisateur via son ID Discord (Mods uniquement)')
+        .addStringOption(option =>
+            option.setName('discordid')
+                .setDescription('L\'ID Discord de l\'utilisateur')
+                .setRequired(true)),
+
+    new SlashCommandBuilder()
+        .setName('poll')
+        .setDescription('Créer un sondage avec plusieurs choix')
+        .addStringOption(option =>
+            option.setName('question')
+                .setDescription('Titre du sondage')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('texte')
+                .setDescription('Description / Détails du sondage')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('nombre_reponses')
+                .setDescription('Nombre de choix d\'options')
+                .setRequired(true)
+                .setMinValue(2)
+                .setMaxValue(10))
+        .addStringOption(option =>
+            option.setName('reponses')
+                .setDescription('Options séparées par des virgules (ex : Oui, Non, Peut-être)')
+                .setRequired(true)),
+
+    new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('Affiche l\'avatar d\'un membre en grand format')
+        .addUserOption(option =>
+            option.setName('membre')
+                .setDescription('Le membre dont tu veux voir l\'avatar')
+                .setRequired(false))
 
 ].map(command => command.toJSON());
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.process ? process.env.TOKEN : '');
 
 (async () => {
     try {
